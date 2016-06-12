547cd6aa710dd21e8ab365aa187504061c9275ae
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2016-06-12 15:56:20 +0700

    Move composer project to msvs subdir

--- msvs/composer/gitver.bat
+++ msvs/composer/gitver.bat
@@ -1,4 +1,4 @@
-set output="..\src\gitver.h"
+set output="..\..\src\gitver.h"
 
 for /f "tokens=*" %%i in ('git describe master') do set version=%%i
 set version_definition=#define GIT_VER ^"%version%^"
